6904964b6fec117ac66ab8a6176bfae5058255ac
yuri-miotto <yurimagrinimiotto@gmail.com>
2023-03-05 10:32:01 -0300

    Add project files.

---
+++ Fatorial/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numero, fatorial, temp;
+        Console.WriteLine("Informe um número...");
+        numero = int.Parse(Console.ReadLine());
+        fatorial = 1;
+
+        if (numero == 0)
+        {
+            fatorial = 1;
+        }
+
+        else
+        {
+            for (int i = 1; i < numero; i++)
+            {
+                temp = numero - i;
+                fatorial *= temp;
+
+            }
+
+            fatorial = fatorial * numero;
+        }
+
+        
+
+        
+
+        Console.WriteLine("o fatorial de " + numero + " é " + fatorial);
+
+        Console.ReadKey();
+
+
+
+    }
+}
